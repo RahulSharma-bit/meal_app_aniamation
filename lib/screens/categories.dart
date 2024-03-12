eb403a21f01a4abdb94b83f5b8f3355b5b2c9a4d
Rahul Sharama <ravigeneralstore161@gmail.com>
2024-03-12 21:27:36 +0530

    Adding Animation using Slide Transition Class

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -73,10 +73,20 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             ),
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
-        child: child,
-      ),
+      // builder: (context, child) => Padding(
+      //   padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      //   child: child,
+      // ), ==============================> animation using Padding
+      //now we will implement using Slide animation as shown below
+      builder: (context, child) => SlideTransition(
+          position: Tween(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),
+          ).animate(
+            CurvedAnimation(
+                parent: _animationController, curve: Curves.easeInOut),
+          ),
+          child: child),
     );
   }
 }
